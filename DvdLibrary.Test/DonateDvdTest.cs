d6ba3e5503eaa4e398e86261a3b6c1dbc870b7c5
muratilgun <muratilgun34@gmail.com>
2021-11-14 17:51:34 +0300

    RentalCopyAddedToMovie

--- DvdLibrary.Test/DonateDvdTest.cs
+++ DvdLibrary.Test/DonateDvdTest.cs
@@ -4,16 +4,25 @@ namespace DvdLibrary
 {
     public class DonateDvdTest
     {
+        private Library _library;
+        private Movie _movie;
+        [SetUp]
+        public void SetUp()
+        {
+            _movie = new Movie();
+            _library = new Library();
+            _library.Donate(_movie);
+        }
         [Test]
         public void DonateDvd()
         {
-            Library library = new Library();
-            Movie movie = new Movie();
-            library.Donate(movie);
-            Assert.True(library.ContainsMovie( movie));
-            Assert.AreEqual(1,movie.Copies);
+            Assert.True(_library.ContainsMovie( _movie));
+            RentalCopyAddedToMovie();
         }
-
-
+        [Test]
+        public void RentalCopyAddedToMovie()
+        {
+            Assert.AreEqual(1, _movie.Copies);
+        }
     }
 }
