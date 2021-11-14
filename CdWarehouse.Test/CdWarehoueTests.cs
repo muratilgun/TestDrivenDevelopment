e241bbe532bf4402efdcfc7016112a7c3036335f
muratilgun <muratilgun34@gmail.com>
2021-11-14 21:46:03 +0300

    CdCreatedWithInitialStock

---
+++ CdWarehouse.Test/CdWarehoueTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace CdWarehouse.Test
+{
+    public class CdWarehoueTests
+    {
+       public class BuyCdTest
+       {
+           [Test]
+           public void CdCreatedWithInitialStock()
+           {
+               CD cd = new CD(10);
+               Assert.AreEqual(10, cd.StockCount);
+           }
+       }
+    }
+
+    public class CD
+    {
+        public CD(int initialStock)
+        {
+            StockCount = initialStock;
+        }
+        public double StockCount { get; private set; }
+    }
+}
