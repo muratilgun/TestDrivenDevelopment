bc3ec7fe503fe33d59b33122f9db1f321bec082f
muratilgun <muratilgun34@gmail.com>
2021-11-15 20:49:17 +0300

    TurnsRightFromNorthToEast

---
+++ MarsRoverDuplication.Test/MarsRoverTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace MarsRoverDuplication.Test
+{
+    public class MarsRoverTest
+    {
+        [Test]
+        public void TurnsRightFromNorthToEast()
+        {
+            Rover rover = new Rover("N");
+            rover.Go("R");
+            Assert.AreEqual("E",rover.Facing);
+        }
+
+    }
+
+    public class Rover
+    {
+        public Rover(string facing)
+        {
+
+        }
+
+        public object Facing => "E";
+
+        public void Go(string instructions)
+        {
+        }
+    }
+    
+}
