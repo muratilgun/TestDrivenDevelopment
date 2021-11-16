b4cd0a740972b3f6d00da596297a9d622c9bbfe9
muratilgun <muratilgun34@gmail.com>
2021-11-16 21:09:51 +0300

    TurnsLeftAntiClockwise

--- MarsRoverDuplication.Test/MarsRoverTest.cs
+++ MarsRoverDuplication.Test/MarsRoverTest.cs
@@ -16,6 +16,17 @@ namespace MarsRoverDuplication.Test
             Assert.AreEqual(endsFacing,rover.Facing);
         }
 
+        [TestCase("N","E")]
+        [TestCase("E","S")]
+        [TestCase("S","W")]
+        [TestCase("W","N")]
+        
+        public void TurnsLeftAntiClockwise(string startsFacing, string endsFacing)
+        {
+            Rover rover = new Rover(startsFacing);
+            rover.Go("L");
+            Assert.AreEqual(endsFacing,rover.Facing);
+        }
         
         [Test]
         public void TurnsRightEastToSouth()
