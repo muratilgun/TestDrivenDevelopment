52d9e5f3e9e08550e783dbc6aef5ba48c8f90d54
muratilgun <muratilgun34@gmail.com>
2021-11-17 23:45:51 +0300

    More and more Test

--- MarsRoverInsideOut/MarsRoverTest.cs
+++ MarsRoverInsideOut/MarsRoverTest.cs
@@ -5,11 +5,56 @@ namespace MarsRoverInsideOut
     [TestFixture]
     public class MarsRoverTest
     {
-        [Test]
-        public void TurnsRightNorthToEast()
+        [TestCase("N","E")]
+        [TestCase("E","S")]
+        [TestCase("S","W")]
+        [TestCase("W","N")]
+        public void TurnsRightClockwise(string startsFacing, string endsFacing)
         {
-            
+            Rover rover = new Rover(startsFacing, new[] { 0, 0 });
+            rover.Right();
+            Assert.AreEqual(endsFacing,rover.Facing);
         }
         
+        [TestCase("N","W")]
+        [TestCase("W","S")]
+        [TestCase("S","E")]
+        [TestCase("E","N")]
+        public void TurnsLeftAntiClockwise(string startsFacing, string endsFacing)
+        {
+            Rover rover = new Rover(startsFacing, new[] { 0, 0 });
+            rover.Left();
+            Assert.AreEqual(endsFacing,rover.Facing);
+        }
+        [TestCase("N",new[]{5,5},new[]{5,4})]
+        [TestCase("E",new[]{5,5},new[]{4,5})]
+        [TestCase("S",new[]{5,5},new[]{5,6})]
+        [TestCase("W",new[]{5,5},new[]{6,5})]
+        public void MovesForwardInDirectionFacing(string facing, int[] startPosition, int[] endPosition)
+        {
+            Rover rover = new Rover(facing, startPosition);
+            rover.Forward();
+            Assert.AreEqual(endPosition,rover.Position);
+        }
+        
+        
+        
+        [TestCase('R',"Right")]
+        [TestCase('L',"Left")]
+        [TestCase('F',"Forward")]
+        [TestCase('B',"Back")]
+        public void FindsActionForInstruction(char instruction, string methodName)
+        {
+            Assert.AreEqual(methodName,new Rover(null,null).FindAction(instruction).Method.Name);
+        }
+
+        [Test]
+        public void ExecutesSequenceOfInstructions()
+        {
+            Rover rover = new Rover("N",new []{5,5});
+            rover.Go("RFF");
+            Assert.AreEqual("E",rover.Facing);
+            Assert.AreEqual(new []{7,5},rover.Position);
+        }
     }
 }
---
+++ MarsRoverInsideOut/Rover.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MarsRoverInsideOut
+{
+    public class Rover
+    {
+        public Rover(string facing, int[] position)
+        {
+            Facing = facing;
+            Position = position;
+        }
+
+        public string Facing { get; private set; }
+        public int[] Position { get; private set; }
+        private void Move(int[][] vectors)
+        {
+            int facing = Array.IndexOf(new[] { "N", "E", "S", "W" }, Facing);
+            int[] vector = vectors[facing];
+            Position[0] += vector[0];
+            Position[1] += vector[1];
+        }
+        public Action FindAction(char instruction)
+        {
+            return new Dictionary<char, Action>()
+            {
+                {'R',Right},
+                {'L',Left},
+                {'F',Forward},
+                {'B',Back}
+            }[instruction];
+        }
+
+        public void Go(string instructions)
+        {
+            List<char> actions = instructions.ToCharArray().ToList();
+            actions.ForEach((action)=> FindAction(action).Invoke());
+        }
+        public void Right()
+        {
+            Turn(new []{"N","E","S","W"});
+        }
+        public void Left()
+        {
+            Turn(new[] { "N", "W", "S", "E" });
+        }
+
+        private void Turn(string[] compass)
+        {
+            int current = Array.IndexOf(compass, Facing);
+            Facing = compass[(current + 1) % 4];
+        }
+
+        public void Forward()
+        {
+            Move(new[]{new []{0,1},new []{1,0},new []{0,-1},new []{-1,0}});
+
+        }
+        public void Back()
+        {         
+            Move(new[]{new []{0,-1},new []{-1,0},new []{0,1},new []{1,0}});
+        }
+    }
+}
