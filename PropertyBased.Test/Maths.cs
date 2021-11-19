f1606bf35dbda3b699057fad2bc7d07ea6778ca9
muratilgun <muratilgun34@gmail.com>
2021-11-19 23:41:47 +0300

    SquareRootTests

---
+++ PropertyBased.Test/Maths.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PropertyBased.Test
+{
+    public class Maths
+    {
+        public double Sqrt(double number)
+        {
+            double root = number / 2;
+            double last = 0;
+            while (Math.Abs(root - last) >  0)
+            {
+                last = root;
+                root = (root + (number / root)) / 2;
+            }
+
+            return root;
+        }
+    }
+}
---
+++ PropertyBased.Test/SquareRootTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace PropertyBased.Test
+{
+    [TestFixture]
+    public class SquareRootTests
+    {
+        [Test]
+        public void RooOf0Is0()
+        {
+            Assert.AreEqual(0.0,new Maths().Sqrt(0.0));
+        }
+
+        [Test]
+        public void RootOf1Is1()
+        {
+            Assert.AreEqual(1.0,new Maths().Sqrt(1.0));
+        }
+        [Test]
+        public void RootOf4Is2()
+        {
+            Assert.AreEqual(2.0,new Maths().Sqrt(4.0));
+        }
+        [Test]
+        public void RootOf9Is3()
+        {
+            Assert.AreEqual(3.0,new Maths().Sqrt(9.0));
+        }
+        [Test]
+        public void RootOf25Is5()
+        {
+            Assert.AreEqual(5.0,new Maths().Sqrt(25.0));
+        }
+    }
+}
