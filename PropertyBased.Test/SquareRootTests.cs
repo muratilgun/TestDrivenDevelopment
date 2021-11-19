26de9269121ea78f0775d675e8c627fa53140cd5
muratilgun <muratilgun34@gmail.com>
2021-11-20 00:00:10 +0300

    1E-12 delta  in AreEqual

--- PropertyBased.Test/SquareRootTests.cs
+++ PropertyBased.Test/SquareRootTests.cs
@@ -9,7 +9,7 @@ namespace PropertyBased.Test
         public void SquareRootOf([Range(0,99)]double number)
         {
             double root = new Maths().Sqrt(number);
-            Assert.AreEqual(number,root * root);
+            Assert.AreEqual(number,root * root , 1E-12);
         }
     }
 }
