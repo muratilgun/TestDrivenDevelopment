ae6c8b33338ecaefca5fa947a629b3c505ebeee1
muratilgun <muratilgun34@gmail.com>
2021-11-21 22:53:20 +0300

    Move Rover

---
+++ MarsRover.Test/Rover.cs
@@ -0,0 +1,84 @@
+﻿using System.Linq;
+
+namespace MarsRover.Test
+{
+    public class Rover
+    {
+        public Rover(string facing, int x, int y)
+        {
+            Position = new[] { x, y };
+            Facing = facing;
+        }
+
+        public int[] Position { get; private set; }
+        public string Facing { get; private set; }
+
+        public void Go(string instructions)
+        {
+            instructions.ToCharArray().ToList().ForEach((instruction) =>
+            {
+                if (instruction == 'R')
+                {
+                    if (Facing == "N")
+                    {
+                        Facing = "E";
+                        return;
+                    }
+                    if (Facing == "E")
+                    {
+                        Facing = "S";
+                        return;
+                    }
+                    if (Facing == "S")
+                    {
+                        Facing = "W";
+                        return;
+                    }
+                    Facing = "N";
+                }
+                if (instruction == 'L')
+                {
+                    if (Facing == "N")
+                    {
+                        Facing = "S";
+                        return;
+                    }
+                    if (Facing == "W")
+                    {
+                        Facing = "S";
+                        return;
+                    }
+                    if (Facing == "S")
+                    {
+                        Facing = "E";
+                        return;
+                    }
+
+                    Facing = "N";
+                }
+                if (instruction == 'F')
+                {
+                    if (Facing == "N")
+                    {
+                        Position = new[] { Position[0], Position[1] + 1 };
+                    }
+
+                    if (Facing == "E")
+                    {
+                        Position = new[] { Position[0] + 1, Position[1] };
+                    }
+
+                    if (Facing == "S")
+                    {
+                        Position = new[] { Position[0], Position[1] - 1 };
+                    }
+
+                    if (Facing == "W")
+                    {
+                        Position = new[] { Position[0] - 1, Position[1] };
+                    }
+                }
+            });
+        }
+    }
+}
--- MarsRover.Test/RoverTests.cs
+++ MarsRover.Test/RoverTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace MarsRover.Test
 {
@@ -12,85 +11,5 @@ namespace MarsRover.Test
             rover.Go("RFF");
             Assert.AreEqual(new[] {7,5}, rover.Position);
         } 
-    }
-
-    public class Rover
-    {
-        public Rover(string facing, int x, int y)
-        {
-            Position = new[] { x, y };
-            Facing = facing;
-        }
-
-        public int[] Position { get; private set; }
-        public string Facing { get; private set; }
-
-        public void Go(string instructions)
-        {
-            instructions.ToCharArray().ToList().ForEach((instruction) =>
-            {
-                if (instruction == 'R')
-                {
-                    if (Facing == "N")
-                    {
-                        Facing = "E";
-                        return;
-                    }
-                    if (Facing == "E")
-                    {
-                        Facing = "S";
-                        return;
-                    }
-                    if (Facing == "S")
-                    {
-                        Facing = "W";
-                        return;
-                    }
-                    Facing = "N";
-                }
-                if (instruction == 'L')
-                {
-                    if (Facing == "N")
-                    {
-                        Facing = "S";
-                        return;
-                    }
-                    if (Facing == "W")
-                    {
-                        Facing = "S";
-                        return;
-                    }
-                    if (Facing == "S")
-                    {
-                        Facing = "E";
-                        return;
-                    }
-
-                    Facing = "N";
-                }
-                if (instruction == 'F')
-                {
-                    if (Facing == "N")
-                    {
-                        Position = new[] { Position[0], Position[1] + 1 };
-                    }
-
-                    if (Facing == "E")
-                    {
-                        Position = new[] { Position[0] + 1, Position[1] };
-                    }
-
-                    if (Facing == "S")
-                    {
-                        Position = new[] { Position[0], Position[1] - 1 };
-                    }
-
-                    if (Facing == "W")
-                    {
-                        Position = new[] { Position[0] - 1, Position[1] };
-                    }
-                }
-            });
-        }
     }
 }
