b28224d52ae386132d9a105c867e54afac16be73
muratilgun <muratilgun34@gmail.com>
2021-11-22 23:58:37 +0300

    refactoring add method

--- StringCalculator/StringCalculator.Test/CalculatorTests.cs
+++ StringCalculator/StringCalculator.Test/CalculatorTests.cs
@@ -18,5 +18,18 @@ namespace StringCalculator.Test
             // Assert
             result.Should().Be(expected);
         }
+        
+        [Theory]
+        [InlineData("1,2,3",6)]
+        [InlineData("10,90,10,20",130)]
+        public void Add_AddsUpToAnyNumber_WhenStringIsValid(string calculation, int expected)
+        {
+            // Arrange
+            var sut = new Calculator();
+            // Act
+            var result = sut.Add(calculation);
+            // Assert
+            result.Should().Be(expected);
+        }
     }
 }
