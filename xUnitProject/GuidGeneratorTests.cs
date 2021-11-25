0de9e4b1f8d80a85d9fc922ea6bdf4cba7c8d8b4
muratilgun <muratilgun34@gmail.com>
2021-11-25 21:38:47 +0300

    GuidGeneratorTests

---
+++ xUnitProject/GuidGeneratorTests.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace xUnitProject
+{
+    public class GuidGeneratorTests
+    {
+        private readonly GuidGenerator _guidGenerator;
+        private readonly ITestOutputHelper _output;
+
+        public GuidGeneratorTests(ITestOutputHelper output)
+        {
+            _guidGenerator = new GuidGenerator();
+            _output = output;
+        }
+
+        [Fact]
+        public void GuidTestOne()
+        {
+            var guid = _guidGenerator.RandomGuid;
+            _output.WriteLine($"The guid was: {guid}");
+        }
+        
+        [Fact]
+        public void GuidTestTwo()
+        {
+            var guid = _guidGenerator.RandomGuid;
+            _output.WriteLine($"The guid was: {guid}");
+        }
+    }
+    public class GuidGeneratorTestsTwo
+    {
+        
+    }
+}
