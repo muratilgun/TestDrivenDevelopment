8ff15b9e460feb92445192ef239998e6c164d7dd
muratilgun <muratilgun34@gmail.com>
2021-11-25 21:49:48 +0300

    CollectionDefinition & ICollectionFixture<GuidGenerator>

--- xUnitProject/GuidGeneratorTests.cs
+++ xUnitProject/GuidGeneratorTests.cs
@@ -3,7 +3,10 @@ using Xunit.Abstractions;
 
 namespace xUnitProject
 {
-    public class GuidGeneratorTests : IClassFixture<GuidGenerator>
+    [CollectionDefinition("Guid generator")]
+    public class GuidGeneratorDefinition : ICollectionFixture<GuidGenerator> { }
+[Collection("Guid generator")]
+    public class GuidGeneratorTests
     {
         private readonly GuidGenerator _guidGenerator;
         private readonly ITestOutputHelper _output;
@@ -28,8 +31,31 @@ namespace xUnitProject
             _output.WriteLine($"The guid was: {guid}");
         }
     }
+    [Collection("Guid generator")]
     public class GuidGeneratorTestsTwo
     {
         
+        private readonly GuidGenerator _guidGenerator;
+        private readonly ITestOutputHelper _output;
+
+        public GuidGeneratorTestsTwo(ITestOutputHelper output, GuidGenerator guidGenerator)
+        {
+            _output = output;
+            _guidGenerator = guidGenerator;
+        }
+
+        [Fact]
+        public void GuidTestOne()
+        {
+            var guid = _guidGenerator.RandomGuid;
+            _output.WriteLine($"The guid was: {guid}");
+        }
+        
+        [Fact]
+        public void GuidTestTwo()
+        {
+            var guid = _guidGenerator.RandomGuid;
+            _output.WriteLine($"The guid was: {guid}");
+        }
     }
 }
